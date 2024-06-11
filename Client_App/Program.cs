e7bae3c5fccfa38861472da220ebc8ac7b7f2357
Mahdi <mahdimaani8@gmail.com>
2024-06-11 05:06:15 -0700

    UsersManagement

---
+++ Client_App/Program.cs
@@ -0,0 +1,47 @@
+﻿using DataLayer.Context;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Repositories;
+using DataLayer.Services;
+using DataLayer;
+
+namespace Client_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyContext db = new MyContext();
+
+            //Person p = new Person()
+            //{
+            //    Name = "Iman",
+            //    Family = "Madaeny",
+            //    website = "Toplearn.com"
+            //};
+            //db.Persons.Add(p);
+            //db.SaveChanges();
+
+            //MyContext db = new MyContext();
+            //IPersonRepository personRepository = new PersonRepository(db);
+            //personRepository.InsertPerson(new Person()
+            //{
+            //    Name = "Ali",
+            //    Family = "Alizadeh",
+            //    website = "Barnamenevisan.org"
+            //});
+            //personRepository.Save();
+
+
+            MyContext db = new MyContext();
+            MyGenericRepository<Person> personRepository = new MyGenericRepository<Person>(db);
+
+            var query = personRepository.Get(p=>p.Name=="Iman");
+            db.Dispose();
+        }
+    }
+}
---
+++ DataLayer/Services/PersonRepository.cs
@@ -0,0 +1,56 @@
+﻿using DataLayer.Context;
+using DataLayer.Models;
+using DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Services
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private MyContext db;
+        public PersonRepository(MyContext context)
+        {
+            db = context;
+        }
+        public void DeletePerson(int PersonId)
+        {
+            var person=GetPersonById(PersonId);
+            DeletePerson(person);
+        }
+
+        public void DeletePerson(Person person)
+        {
+           db.Entry(person).State = EntityState.Deleted;
+        }
+
+        public List<Person> GetAllPerson()
+        {
+            return db.Persons.ToList();
+        }
+
+        public Person GetPersonById(int PersonId)
+        {
+            return db.Persons.Find(PersonId);
+        }
+
+        public void InsertPerson(Person person)
+        {
+            db.Persons.Add(person);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            db.Entry(person).State = EntityState.Modified;
+        }
+    }
+}
